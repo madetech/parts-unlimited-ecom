3be356471d2241119a99679b58cf261aec2d3506
rebecca fitzsimmons <rebecca@madetech.com>
2018-09-06 11:07:12 +0100

    add item_total method to items domain object

--- lib/domain/items.rb
+++ lib/domain/items.rb
@@ -2,10 +2,11 @@ class Items
   attr_accessor :part_id,
                 :part_name,
                 :part_price,
-                :part_quantity
+                :part_quantity,
+                :part_total
 
   def item_total
     part_price * part_quantity
   end 
-  
+   
 end
