9499db13cafa7db02129c47ad30921b9c2baa482
rebecca fitzsimmons <rebecca@madetech.com>
2018-09-10 10:44:53 +0100

    Create Items domain object

--- spec/unit/file_items_gateway_spec.rb
+++ spec/unit/file_items_gateway_spec.rb
@@ -12,8 +12,19 @@ describe FileItemsGateway do
   end
 
   it 'can get a row of item details' do
-    items = [{ part_id: '108', part_name: 'Jeremy', part_price: '28.00', part_quantity: '1' }]
-    file_items_gateway.save(items)
-    expect(file_items_gateway.all).to eq([{ part_id: '108', part_name: 'Jeremy', part_price: '28.00', part_quantity: '1' }])
+    item_builder = ItemBuilder::Item.new
+    item_builder.items = [{
+      id: '108', name: 'Jeremy', price: '28.00', quantity: '1'
+    }]
+    item = item_builder.build
+
+    file_items_gateway.save(item)
+
+    file_items_gateway.all.first.tap do |items|
+      expect(items[0].id).to eq('108')
+      expect(items[0].name).to eq('Jeremy')
+      expect(items[0].price).to eq('28.00')
+      expect(items[0].quantity).to eq('1')
+    end
   end
 end
