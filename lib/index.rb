def6ca244f76cbee70aa15665460d569b281d5ae
Emma Corbett <emma@madetech.com>
2018-09-21 10:55:10 +0100

    validate that items have been added before continuing

--- lib/index.rb
+++ lib/index.rb
@@ -151,16 +151,17 @@ post '/shipping-total' do
   @order_details = { shipping_total: shipping_total }
   save_order_details = SaveOrderDetails.new(order_gateway: @order_gateway)
   response = save_order_details.execute(order_details: @order_details)
-  
   summary = @view_summary.execute
   @net_total = summary[:net_total]
   @vat = summary[:vat_total]
   @items = summary[:items]
   @errors = []
-
   @order_errors = response[:errors]
+  if summary[:items].empty?
+    @errors.push(:no_items)
+    return erb :items_details
+  end
   redirect '/order-summary' if response[:successful]
-  
   erb :items_details
 end
 
